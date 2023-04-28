ad6ff1ac3744bc592ecf2372906fa2eb1b797393
Jerry Zhou <81694589+je-zhou@users.noreply.github.com>
2023-04-28 21:51:25 +1000

    Initial UI setup

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import styles from "./Home.module.css";
+import Navbar from '@components/Navbar/Navbar';
+import Body from '@components/Body/Body';
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<h1 className={styles.title}>Blinq</h1>
+			<Navbar></Navbar>
+			<Body></Body>
+
+			{/* <h1 className={styles.title}>Blinq</h1>
 
 			<p className={styles.description}>Manage your integrations here</p>
 
-			<div className={styles.grid}>Build here</div>
+			<div className={styles.grid}>Build here</div> */}
 		</main>
 	)
 }
--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,10 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Database, User } from "../../database";
+import { Database, User } from "../../../database";
 
 export default function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse<User>
+	_req: NextApiRequest,
+	res: NextApiResponse<User>
 ) {
-  res.status(200).json(Database.getUser());
+	res.status(200).json(Database.getUser());
 }
