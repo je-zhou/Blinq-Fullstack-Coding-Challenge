ea2305a58ad3e811b3145804215b0c8f895ff221
Jerry Zhou <81694589+je-zhou@users.noreply.github.com>
2023-04-29 00:47:00 +1000

    requiredParams and getParamsList tested

--- src/utils/integrationPartners.test.ts
+++ src/utils/integrationPartners.test.ts
@@ -10,3 +10,41 @@ test("Integration Factory returns the correct Integration Partners", () => {
 	expect(integrationPartners).toContainEqual(hubspot);
 });
 
+
+
+describe("Salesforce Integration Class working as intended", () => {
+	const salesforce = new SalesforceIntegration();
+
+	test("Salesforce required parameters are client_id and client_secret", () => {
+		const params = salesforce.getParamsList();
+
+		expect(params).toContain("client_id");
+		expect(params).toContain("client_secret");
+		expect(params.length).toBe(2);
+	});
+})
+
+describe("Zapier Integration Class working as intended", () => {
+	const zapier = new ZapierIntegration();
+
+	test("Zapier required parameter is api_key", () => {
+		const params = zapier.getParamsList();
+
+		expect(params).toContain("api_key");
+		expect(params.length).toBe(1);
+	});
+})
+
+describe("Hubspot Integration Class working as intended", () => {
+	const hubspot = new HubspotIntegration();
+
+	test("Hubspot required parameters are tenant_domain, client_id, client_secret, and field_mappings", () => {
+		const params = hubspot.getParamsList();
+
+		expect(params).toContain("tenant_domain");
+		expect(params).toContain("client_id");
+		expect(params).toContain("client_secret");
+		expect(params).toContain("field_mappings");
+		expect(params.length).toBe(4);
+	});
+})
