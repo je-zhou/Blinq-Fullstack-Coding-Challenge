f3780328751bd4845399b8a3fe22a64f03c68549
Jerry Zhou <81694589+je-zhou@users.noreply.github.com>
2023-04-30 21:42:39 +1000

    Adding a loading indicator to the the connect button

--- src/common/components/IntegrationTile/IntegrationTileSubMenu.tsx
+++ src/common/components/IntegrationTile/IntegrationTileSubMenu.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./IntegrationTileSubMenu.module.css";
 import buttonStyles from "@styles/Button.module.css"
 import SubMenuField from './SubMenuField';
+import LoadingIndicator from '@components/LoadingIndicator/LoadingIndicator';
 
 
 interface IIntegrationTileSubMenu {
@@ -11,12 +12,26 @@ interface IIntegrationTileSubMenu {
 export default function IntegrationTileSubMenu({ requiredParams }: IIntegrationTileSubMenu) {
 	const fields = requiredParams.map((field) => <SubMenuField fieldName={field} />)
 
+	const [isLoading, setIsLoading] = useState(false);
+
+	function onClick() {
+		setIsLoading(true);
+
+		setTimeout(() => {
+			console.log('Mocking a delay to connect with the integration partner');
+			setIsLoading(false);
+		}, 1000);
+	}
+
 	return (
 		<div className={styles.subMenuContainer}>
 			{fields}
-			<button className={`${buttonStyles.button} ${buttonStyles.connect}`}>
-				Connect
+			<button
+				className={`${buttonStyles.button} ${isLoading ? buttonStyles.loading : buttonStyles.connect}`}
+				onClick={onClick}>
+				{isLoading ? <LoadingIndicator/> : "Connect"}
 			</button>
 		</div>
 	)
 }
+
---
+++ src/common/components/LoadingIndicator/LoadingIndicator.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './LoadingIndicator.module.css'
+
+export default function LoadingIndicator() {
+	return (
+		<div className={styles.spinner}></div>
+	)
+}
