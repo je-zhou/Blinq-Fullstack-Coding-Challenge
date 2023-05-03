238d4593889c903591946ad22b4f0ef5d74fcb3c
Jerry Zhou <81694589+je-zhou@users.noreply.github.com>
2023-05-04 00:18:09 +1000

    reduce nesting in apiRoute

--- src/pages/api/integrations/[id].ts
+++ src/pages/api/integrations/[id].ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { IntegrationClient } from "@utils/integrationClients/integrationClient";
 import { IntegrationClientsFactory } from "@utils/integrationClients/integrationClientFactory";
-import { Database } from "database";
+import { Contact, Database } from "database";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
@@ -19,31 +20,14 @@ export default async function handler(
 
 			const client = IntegrationClientsFactory.getClientByID(id); // Get the right Integration Client based on id passed to the API route, if no client then throws error
 
-			// Check if the id passed in is a valid Integration Partner
 			if (body.type === "connect") {
-
-				const connectionOutcome = await client.connect(body.params, body.contacts);
-
-				// If success, return status 200
-				if (connectionOutcome) {
-					res.status(200).json({ outcome: "Connection Success!: Integration Complete" });
-				} else {
-					// If fail due to client side error, return status 402
-					res.status(402).json({ outcome: "Connection Failed: Client Side Error" })
-				}
-			} else if (body.type === "disconnect") {
-
-				const connectionOutcome = await client.disconnect(body.params);
-
-				// If success, return status 200
-				if (connectionOutcome) {
-					res.status(200).json({ outcome: "Successfully Disconnected" });
-				} else {
-					// If fail due to client side error, return status 403
-					res.status(404).json({ outcome: "Failed to Disconnect: Client Side Error" })
-				}
-			} else {
-				res.status(406).json({ outcome: "Error: Invalid connection type. Please pass either 'connect' or 'disconnect' as the value for the key 'type'" })
+				await connectToClient(client, body.params, body.contacts, res);
+			}
+			else if (body.type === "disconnect") {
+				await disconnectFromClient(client, body.params, res);
+			}
+			else {
+				res.status(406).json({ outcome: "Error: Invalid connection type. Please pass either 'connect' or 'disconnect' as the value for the key 'type'" });
 			}
 
 			break;
@@ -51,5 +35,30 @@ export default async function handler(
 			res.setHeader('Allow', ['POST']);
 			res.status(403).end(`Method ${method} Not Allowed`);
 			break;
+	}
+}
+
+async function connectToClient(client: IntegrationClient, params: { [key: string]: any }, contacts: Array<Contact>, res: NextApiResponse<Data>) {
+	const connectionOutcome = await client.connect(params, contacts);
+
+	// If success, return status 200
+	if (connectionOutcome) {
+		res.status(200).json({ outcome: "Connection Success!: Integration Complete" });
+	} else {
+		// If fail due to client side error, return status 402
+		res.status(402).json({ outcome: "Connection Failed: Client Side Error" });
+	}
+}
+
+
+async function disconnectFromClient(client: IntegrationClient, params: { [key: string]: any }, res: NextApiResponse<Data>) {
+	const connectionOutcome = await client.disconnect(params);
+
+	// If success, return status 200
+	if (connectionOutcome) {
+		res.status(200).json({ outcome: "Successfully Disconnected" });
+	} else {
+		// If fail due to client side error, return status 403
+		res.status(404).json({ outcome: "Failed to Disconnect: Client Side Error" });
 	}
 }
